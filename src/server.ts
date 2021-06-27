f9456e29c92310adc350b8eff5a3c9bd1e48bd22
DaianeM <daianersm2@gmail.com>
2021-06-27 16:15:32 -0300

    Cors habilitado

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import "reflect-metadata";
 import "express-async-errors";
 import express, { Request, Response, NextFunction } from 'express';
 import { router } from './routes';
+import cors from 'cors';
 
 import "./database";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
